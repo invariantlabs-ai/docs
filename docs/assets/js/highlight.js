1036f0c1c7624ef0f60918c9c62dd1068881e216
Marc Fischer <mail@marcfischer.at>
2025-04-07 18:08:35 +0200

    highlighting enabled

--- docs/assets/js/highlight.js
+++ docs/assets/js/highlight.js
@@ -25,9 +25,10 @@ function changeElements(codeElements, endpoint) {
 
 document.addEventListener('DOMContentLoaded', function() {
     // check if BASE_URL is defined and reachable
-    fetch(`${BASE_URL}`)
+    fetch(`${BASE_URL}embed/traceview`)
     .then(response => {
         if (!response.ok) {
+            console.log('Network response was not ok');
             throw new Error('Network response was not ok');
         }
         return response.text();
